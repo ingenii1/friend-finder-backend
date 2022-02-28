7e3a9fae628e1a3d8654dd6edf21abe8902bde18
srikar chavali <chavalisrikar@gmail.com>
2022-02-28 13:06:49 +0000

    added deployment check

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5001
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req,res)=> res.send('Deployed successfully') )
+
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
